dd900f3e5cca7173dceec1b86942daf5386931dc
cedricbahirwe <cedricaganzebahirwe@gmail.com>
2021-10-15 10:28:31 +0200

    Implement Encapsulation using access control

--- PokemonBattle/Models/Pokemon.swift
+++ PokemonBattle/Models/Pokemon.swift
@@ -10,10 +10,23 @@ import SwiftUI
 struct Pokemon {
     let img: String
     let name: String
-    var pokemonWidth: CGFloat = 100.0
-    var progressPositon = 0.1
-    var position: CGFloat = 150
-    var hpStatus: HPStatus = HPStatus()
+    private(set) var pokemonWidth: CGFloat = 100.0
+//    private(set) var progressPositon = 0.1
+    private(set) var position: CGFloat = 150
+    private(set) var hpStatus: HPStatus
+    
+    init(img: String,
+         name: String,
+         pokemonWidth: CGFloat = 100.0,
+         position: CGFloat = 150,
+         hpStatus: HPStatus = HPStatus()) {
+        
+        self.img = img
+        self.name = name
+        self.pokemonWidth = pokemonWidth
+        self.position = position
+        self.hpStatus = hpStatus
+    }
     
     mutating func attacked() {
         hpStatus.hited()
@@ -24,11 +37,11 @@ struct Pokemon {
 }
 
 struct HPStatus {
-    var attempts: Int = 0
-    var hpLevel: CGFloat = 150.0
-    var hpColor: Color = .green
+    private(set) var attempts: Int = 0
+    private(set) var hpLevel: CGFloat = 150.0
+    private(set) var hpColor: Color = .green
     private var defeated = false
-    
+
     mutating func hited() {
         hpLevel -= Double.random(in: 1...50)
         attempts += 1
